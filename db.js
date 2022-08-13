e2a8f4172d766a24baa25c4ff3e3abe5259706fc
Alexandr Kolomytski <alexandr.kolom.web@yandex.ru>
2022-08-13 13:46:49 +1000

    fds

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize')
 console.log(process.env.DATABASE_URL)
-module.exports = new Sequelize(process.env.DATABASE_URL, { dialect: 'postgres', port: 5432 })
+module.exports = new Sequelize(`${process.env.DATABASE_URL}`)
 
 
 
