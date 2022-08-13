50a6222996c855006ce5ab6395bfc5bcbf4679fa
Alexandr Kolomytski <alexandr.kolom.web@yandex.ru>
2022-08-13 15:32:31 +1000

    vfds

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize')
-console.log(process.env.DATABASE_URL, process.env)
-module.exports = new Sequelize('dd3snj86qjndrr', 'vcpnllqwfkhjey', 'b7e06606ed2c58a5c7803ba2da84548ef829e3c1484be17e0ced1e83b7c047e7', {
-	host: 'b7e06606ed2c58a5c7803ba2da84548ef829e3c1484be17e0ced1e83b7c047e7',
+console.log(process.env)
+module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
 	dialect: 'postgres',
-	port: 5432
+	port: process.env.DB_PORT,
+	host: process.env.DB_HOST
 })
 
 
