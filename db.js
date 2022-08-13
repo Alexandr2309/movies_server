107e5a5eeec5b7c57b6a9aa7fca09cb0a296c757
Alexandr Kolomytski <alexandr.kolom.web@yandex.ru>
2022-08-13 13:35:37 +1000

    seq

--- db.js
+++ db.js
@@ -1,22 +1,6 @@
-// const { Sequelize } = require('sequelize')
-//
-// module.exports = new Sequelize(
-// 	process.env.DB_NAME,
-// 	process.env.DB_USER,
-// 	process.env.DB_PASSWORD,
-// 	{
-// 		dialect: 'postgres',
-// 		host: process.env.DB_HOST,
-// 		port: process.env.DB_PORT
-// 	}
-// )
-const { Client } = require('pg')
+const { Sequelize } = require('sequelize')
 
-module.exports = new Client({
-	connectionString: process.env.DATABASE_URL,
-	ssl: {
-		rejectUnauthorized: false
-	}
-}).connect()
+module.exports = new Sequelize(process.env.DATABASE_URL)
+
 
 
