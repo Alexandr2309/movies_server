43f05b9817098eb8f74c9bab6b37ad39c07b2051
Alexandr Kolomytski <alexandr.kolom.web@yandex.ru>
2022-08-13 20:46:27 +1000

    fds

--- db.js
+++ db.js
@@ -1,11 +1,17 @@
 const { Sequelize } = require('sequelize')
 console.log(process.env)
 
+
 module.exports = new Sequelize(process.env.DATABASE_URL, {
 	dialect: 'postgres',
-	ssl: true,
 	host: 'localhost',
-	port: 5432
+	port: 5432,
+	dialectOptions: {
+		ssl: {
+			require: true, // This will help you. But you will see nwe error
+			rejectUnauthorized: false // This line will fix new error
+		}
+	}
 })
 
 
