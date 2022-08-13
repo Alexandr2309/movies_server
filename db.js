8e59795e79c4cc330d8cd4d46ff408a331eba697
Alexandr Kolomytski <alexandr.kolom.web@yandex.ru>
2022-08-13 16:44:37 +1000

    fds

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require('sequelize')
 console.log(process.env.DATABASE_URI)
 
-const connectStr = process.env.DATBASE_URI
-console.log('Connection String = ', connectStr)
+const connectString = process.env.DATBASE_URI
+console.log('Connection String = ', connectString)
 console.log('Connection KEY = ', process.env.SECRET_KEY)
 
-module.exports = new Sequelize(connectStr, {
+module.exports = new Sequelize(connectString, {
 	dialect: 'postgres'
 })
 
