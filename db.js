bdd794b1602d6d9fed7bed1c3444ff856b361589
Alexandr Kolomytski <alexandr.kolom.web@yandex.ru>
2022-08-13 16:46:31 +1000

    fdsf

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize')
 console.log(process.env.DATABASE_URI)
 
-const connectString = process.env.DATBASE_URI
+const connectString = process.env.DATBASE_URL
 console.log('Connection String = ', connectString)
 console.log('Connection KEY = ', process.env.SECRET_KEY)
 
